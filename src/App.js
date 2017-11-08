f642406f28e111d8c930d47d140baaf62e94d50a
Brian Phillips <brian.phillips@mindbodyonline.com>
2017-11-07 20:43:30 -0800

    building basic layout and pulling data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import fire from './utils/fire';
+import Header from './components/header';
 
 class App extends Component {
+  state = {
+    isLoading: true,
+    glossary: []
+  }
+
+  componentDidMount() {
+    const glossary = fire.database().ref('glossary');
+    const _this = this;
+
+    glossary.on('value', function(snapshot) {
+      _this.setState({ 
+        glossary: snapshot.val(),
+        isLoading: false
+      })
+    });
+  }
+
   render() {
+    const { glossary } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header/>
+        <div className="term-list">
+          {glossary.map((letter) => (
+            <div key={letter.letter} className="letter-group">
+              <h3>{letter.letter}</h3>
+
+              <ul className="letter-terms">
+                {letter.terms.map((t) => (
+                  <li key={t.term}>
+                    <h4>{t.term}</h4>
+                    <p>{t.definition}</p>
+                    {t.citation}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
