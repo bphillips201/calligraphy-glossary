47f48a766a498d8a8f54f45f4903003f8b06a9b0
Brian Phillips <brian.phillips@SL-ML-0001459.local>
2017-11-10 21:12:05 -0800

    added basic styling for header

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,20 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <header className="App-header wrapper">
-        <h1 className="App-title">Calligraphy Glossary</h1>
+      <header className="header">
+        <div className="row middle-xs header__sub">
+          <div className="header__author col-sm"><a target="_blank" rel="noopener noreferrer" href="http://www.ashleephillips.com">Ashlee Phillips</a></div>
+
+          <div className="header__social col-sm align-right">
+            <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/ashleeletters"><span className="social-cta">Follow on Twitter</span><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            <a target="_blank" rel="noopener noreferrer" href="https://instagram.com/ashleeletters"><span className="social-cta">Follow on Instagram</span><i class="fa fa-instagram" aria-hidden="true"></i></a>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UC8gslRpdQNU7nJKpASu8NTg"><span className="social-cta">Subscribe to my channel</span><i class="fa fa-youtube" aria-hidden="true"></i></a>
+          </div>
+        </div>
+
+        <div className="header__hero wrapper">
+          <h1>Calligraphy Glossary</h1>
+        </div>
       </header>
     )
   }
